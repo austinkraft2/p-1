038651c263ad05484181f207953ac1e619d39f07
austinkraft2 <163015784+austinkraft2@users.noreply.github.com>
2024-03-13 10:42:40 -0500

    3/13

--- p93a/p93a/MainForm.py
+++ p93a/p93a/MainForm.py
@@ -19,6 +19,9 @@ class MainForm(Form):
 		self._label8 = System.Windows.Forms.Label()
 		self._label9 = System.Windows.Forms.Label()
 		self._label10 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
 		self.SuspendLayout()
 		# 
 		# label1
@@ -101,9 +104,46 @@ class MainForm(Form):
 		self._label10.Size = System.Drawing.Size(100, 23)
 		self._label10.TabIndex = 9
 		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.Orange
+		self._button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+		self._button1.Location = System.Drawing.Point(370, 108)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(87, 47)
+		self._button1.TabIndex = 10
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.Orange
+		self._button2.Location = System.Drawing.Point(370, 182)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(87, 47)
+		self._button2.TabIndex = 11
+		self._button2.Text = "clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.Orange
+		self._button3.Location = System.Drawing.Point(370, 258)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(87, 47)
+		self._button3.TabIndex = 12
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
 		# MainForm
 		# 
+		self.BackColor = System.Drawing.Color.Bisque
 		self.ClientSize = System.Drawing.Size(470, 552)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
 		self.Controls.Add(self._label10)
 		self.Controls.Add(self._label9)
 		self.Controls.Add(self._label8)
@@ -118,3 +158,12 @@ class MainForm(Form):
 		self.Text = "p93a"
 		self.ResumeLayout(False)
 
+
+	def Button1Click(self, sender, e):
+		pass
+
+	def Button2Click(self, sender, e):
+		
+
+	def Button3Click(self, sender, e):
+		pass
