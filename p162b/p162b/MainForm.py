5253785c79a35cfd11083451eb98a92d5cc15847
austinkraft2 <163015784+austinkraft2@users.noreply.github.com>
2024-03-27 10:28:29 -0500

    3/27

--- p162b/p162b/MainForm.py
+++ p162b/p162b/MainForm.py
@@ -17,44 +17,55 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
+		self._listBox1.BackColor = System.Drawing.Color.IndianRed
+		self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 9, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 15
 		self._listBox1.Location = System.Drawing.Point(4, 14)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(431, 303)
+		self._listBox1.Size = System.Drawing.Size(456, 289)
 		self._listBox1.TabIndex = 0
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(12, 371)
+		self._button1.BackColor = System.Drawing.Color.Chocolate
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(4, 309)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(100, 56)
+		self._button1.Size = System.Drawing.Size(148, 92)
 		self._button1.TabIndex = 1
 		self._button1.Text = "Calculate"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(157, 371)
+		self._button2.BackColor = System.Drawing.Color.Chocolate
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(158, 309)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(100, 56)
+		self._button2.Size = System.Drawing.Size(148, 92)
 		self._button2.TabIndex = 2
 		self._button2.Text = "Cear"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(301, 371)
+		self._button3.BackColor = System.Drawing.Color.Chocolate
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(312, 309)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(100, 56)
+		self._button3.Size = System.Drawing.Size(148, 92)
 		self._button3.TabIndex = 3
 		self._button3.Text = "Exit"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(447, 511)
+		self.BackColor = System.Drawing.Color.LightSalmon
+		self.ClientSize = System.Drawing.Size(464, 408)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
@@ -74,4 +85,7 @@ class MainForm(Form):
 			pop*= 1.029
 
 	def Button2Click(self, sender, e):
-		self._listBox.Items.Clear()
+		self._listBox1.Items.Clear()
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
